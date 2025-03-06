5e7bd55a8c2df83401cbab82e2b38a8e17cccf51
ManishPatidar <manishpatidar1180@gmail.com>
2025-03-07 00:01:08 +0530

    Fix node engine version

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  build: {
+    outDir: "build"  // Change Vite's output folder to "build"
+  },
   plugins: [react()],
   resolve: {
     alias: {
